18916712a3be56a45bfc69ae181ffffa92634134
Veno <joshwad178@outlook.com>
2023-11-01 13:18:55 -0500

    fix

--- app/tests.py
+++ app/tests.py
@@ -3,16 +3,16 @@ from django.test import SimpleTestCase
 # Create your tests here.
 class NearOneHundred(SimpleTestCase):
     def test_NearOneHundred_Ten(self):
-        response = self.client.get("/warmup-1/near-hundred/10")
-        self.assertContains(response, False)
+        response = self.client.get("/warmup-1/near-hundred/93")
+        self.assertContains(response, True)
 
     def test_NearHundred_90(self):
         response = self.client.get("/warmup-1/near-hundred/90")
         self.assertContains(response, True)
 
     def test_NearHundred_110(self):
-        response = self.client.get("/warmup-1/near-hundred/110")
-        self.assertContains(response, True)
+        response = self.client.get("/warmup-1/near-hundred/89")
+        self.assertContains(response, False)
 
 class StringSplosion(SimpleTestCase):
     def test_string_splosion_code(self):
@@ -29,15 +29,15 @@ class StringSplosion(SimpleTestCase):
 
 class CatDog(SimpleTestCase):
     def test_cat_dog_catcatdog(self):
-        response = self.client.get("/string-2/cat-dog/catcatdog")
-        self.assertContains(response, False)
+        response = self.client.get("/string-2/cat-dog/catdog")
+        self.assertContains(response, True)
 
     def test_cat_dog_dogdogcat(self):
-        response = self.client.get("/string-2/cat-dog/dogdogcat")
+        response = self.client.get("/string-2/cat-dog/catcat")
         self.assertContains(response,False)
     
     def test_cat_dog_dogdogcatcatatogat(self):
-        response = self.client.get("/string-2/cat-dog/dogdogcatcatatogat")
+        response = self.client.get("/string-2/cat-dog/1cat1cadodog")
         self.assertContains(response,True)
 
 class LoneSum(SimpleTestCase):
@@ -46,8 +46,8 @@ class LoneSum(SimpleTestCase):
         self.assertContains(response, 6)
 
     def test_lone_sum_2_2_3(self):
-        response = self.client.get("/logic-2/lone-sum/2/2/3")
-        self.assertContains(response, 3)
+        response = self.client.get("/logic-2/lone-sum/3/2/3")
+        self.assertContains(response, 2)
 
     def test_lone_sum_3_3_3(self):
         response = self.client.get("/logic-2/lone-sum/3/3/3")
--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from django.http.response import HttpResponse
 from django.http.request import HttpRequest
 
-def near_hundred_view(request, num):
+def near_hundred_view(request:HttpRequest, num:int) -> bool:
     if (num in range(90, 111)) or (num in range(190, 211)):
         return HttpResponse(True)
     return HttpResponse(False)
 
-def string_splosion_view(request, word):
+def string_splosion_view(request:HttpRequest, word:str) -> str:
     newWord = ''
     count = 1
     while count <= len(word):
@@ -14,10 +14,10 @@ def string_splosion_view(request, word):
         count += 1
     return HttpResponse(newWord)
 
-def cat_dog_view(request, word):
+def cat_dog_view(request:HttpRequest, word:str) -> str:
     return HttpResponse(word.count('dog') == word.count('cat'))
 
-def lone_sum_view(request, a, b, c):
+def lone_sum_view(request:HttpRequest, a:int, b:int, c:int) -> int:
     if a == b and b == c:
         return HttpResponse(0)
     elif a == b:
